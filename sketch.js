85c68b5d9fe1cf003022b635915fb33df0b68e23
siddhant15oo <61053704+siddhant15oo@users.noreply.github.com>
2020-03-22 20:11:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+var stick
+
+
+  createCanvas(800,400);
+  stick =createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255); 
+  cet=rect (400,200,20,80); 
+  cet2=rect (378, 200, 20,80);
+  cet3=rect (275, 120, 25,165);
+  cet4=rect (500, 120, 25, 165);
+  cet5=rect (250, 100, 295, 25);
+  
+  drawSprites();
+}
